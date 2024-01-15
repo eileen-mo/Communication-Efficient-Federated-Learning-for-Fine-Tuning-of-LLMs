083ae32fc45115c13ad5b14919f820c2b53ab5f8
Eileen <eileen@EileendeMacBook-Pro.local>
2024-01-15 10:00:00 -0800

    Initial commit

---
+++ multigpu/run-lora.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ $# -lt 1 ]]
+then
+    echo "specify number of clients: $0 numclients"
+    return 1
+fi
+NUM_CLIENTS="$1"
+ARGS="${@:2}"
+NUM_GPUS=$(nvidia-smi -L | wc -l)
+
+trap "pkill -P $$" SIGINT
+
+echo "Starting server"
+CUDA_VISIBLE_DEVICES=0 python3 server.py --num_clients $NUM_CLIENTS --rank 0 $ARGS  & 
+sleep 3
+
+for (( i=1; i<=$NUM_CLIENTS; i++ ))
+do
+    echo "Starting client $i"
+    n=$(($i%$NUM_GPUS))
+    CUDA_VISIBLE_DEVICES=$n python3 lora-client.py --num_clients $NUM_CLIENTS --rank $i $ARGS &
+done
+
+wait
